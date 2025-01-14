c3b4e4d5f2adc84dbfea4334b20867a9031a7a6c
Steven Wu <ZhuohengWu@users.noreply.github.com>
2025-01-14 10:39:13 -0500

    Add dbcontext and Connect to database

---
+++ StaffTrackApp/ServerLibrary/Data/StaffTrackDb.cs
@@ -0,0 +1,26 @@
+﻿using BaseLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLibrary.Data;
+
+public class StaffTrackDb : DbContext
+{
+    public StaffTrackDb(DbContextOptions<StaffTrackDb> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Branch> Branches { get; set; }
+    public DbSet<Town> Towns { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+}
