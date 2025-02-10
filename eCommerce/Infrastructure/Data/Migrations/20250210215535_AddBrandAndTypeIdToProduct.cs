029ddac9042b2cc21ea6ce7dd3dbb587a32caca3
Steven Wu <ZhuohengWu@users.noreply.github.com>
2025-02-10 16:56:47 -0500

    Add brand and type id to product

---
+++ eCommerce/Infrastructure/Data/Migrations/20250210215535_AddBrandAndTypeIdToProduct.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBrandAndTypeIdToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductBrandId",
+                table: "Products",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductTypeId",
+                table: "Products",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductBrandId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductTypeId",
+                table: "Products");
+        }
+    }
+}
