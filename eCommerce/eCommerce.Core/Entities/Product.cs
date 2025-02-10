92c8973a28d36b5e9c3e7981e3a4d57cb04c6ab1
Steven Wu <ZhuohengWu@users.noreply.github.com>
2025-02-10 17:06:05 -0500

    Add columns to products

--- eCommerce/eCommerce.Core/Entities/Product.cs
+++ eCommerce/eCommerce.Core/Entities/Product.cs
@@ -3,9 +3,9 @@
     public class Product : BaseEntity
     {
         public string Name { get; set; } = string.Empty;
-        //public string Description { get; set; } = string.Empty;
-        //public decimal Price { get; set; }
-        //public string PictureUrl { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; } = string.Empty;
         public ProductType ProductType { get; set; } = new();
         public int ProductTypeId { get; set; }
         public ProductBrand ProductBrand { get; set; } = new();
