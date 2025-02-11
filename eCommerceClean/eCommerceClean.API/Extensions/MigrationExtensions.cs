95eed9cea4d63eab1b41395b5ae4c4be1ab39c51
Steven Wu <ZhuohengWu@users.noreply.github.com>
2025-02-10 19:48:46 -0500

    Update db migration extensions

--- eCommerceClean/eCommerceClean.API/Extensions/MigrationExtensions.cs
+++ eCommerceClean/eCommerceClean.API/Extensions/MigrationExtensions.cs
@@ -5,10 +5,18 @@ namespace eCommerceeCommerceClean.API.Extensions;
 
 public static class MigrationExtensions
 {
-    public static void ApplyMigrations(this IApplicationBuilder app)
+    public static async void ApplyMigrations(this IApplicationBuilder app)
     {
         using IServiceScope scope = app.ApplicationServices.CreateScope();
-        using StoreDbContext dbContext = scope.ServiceProvider.GetRequiredService<StoreDbContext>();
-        dbContext.Database.Migrate();
+        
+        try
+        {
+            using StoreDbContext dbContext = scope.ServiceProvider.GetRequiredService<StoreDbContext>();
+            await dbContext.Database.MigrateAsync();
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occured during migration");
+        }
     }
 }
