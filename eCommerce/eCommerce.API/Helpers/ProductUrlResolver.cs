a721c3e507688851f6916d9e3c24596b0b55e5b0
Steven Wu <ZhuohengWu@users.noreply.github.com>
2025-02-12 09:12:06 -0500

    map product image url

---
+++ eCommerce/eCommerce.API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using eCommerce.API.Dtos;
+using eCommerce.Core.Entities;
+
+namespace eCommerce.API.Helpers
+{
+    public class ProductUrlResolver(IConfiguration Config) : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl)) 
+            { 
+                return Config["ApiUrl"] + source.PictureUrl;
+            }
+
+            return string.Empty;
+        }
+    }
+}
