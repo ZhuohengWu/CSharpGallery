54cc0d04ad988e7400fa0053d37b10a2b025f5fd
Steven Wu <ZhuohengWu@users.noreply.github.com>
2025-04-01 11:10:57 -0400

    Add product component

---
+++ SwiftShop/swiftshop-angular-app/src/app/products/products.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-products',
+  imports: [CommonModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss'
+})
+export class ProductsComponent {
+  products = [
+    { id: 1, name: 'Product 1', price: 10 },
+    { id: 2, name: 'Product 2', price: 20 },
+    { id: 3, name: 'Product 3', price: 30 }
+  ];
+}
