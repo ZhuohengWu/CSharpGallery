0fe041ecc59e80a125f3129d1165d72dbab9872d
Steven Wu <ZhuohengWu@users.noreply.github.com>
2025-04-02 14:27:15 -0400

    add tailwind css

--- SwiftShop/swiftshop-angular-app/src/app/app.component.ts
+++ SwiftShop/swiftshop-angular-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 import { ProductsComponent } from "./products/products.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductsComponent],
+  imports: [RouterOutlet, ProductsComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
